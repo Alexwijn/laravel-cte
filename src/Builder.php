da5c158f2f385e6afa0687459e14d2bd165448e3
Alex Wijnholds <alex.wijnholds@pos4.nl>
2019-07-17 11:55:06 +0200

    Added missing value method

--- src/Builder.php
+++ src/Builder.php
@@ -193,6 +193,21 @@ class Builder implements Constrainable
     }
 
     /**
+     * Get a single column's value from the first result of a query.
+     *
+     * @param  string  $column
+     * @return mixed
+     */
+    public function value($column)
+    {
+        if ($result = $this->first([$column])) {
+            return $result->{$column};
+        }
+
+        return null;
+    }
+
+    /**
      * Pluck a single column's value from the first result of a query.
      *
      * @param  string $column
@@ -202,7 +217,7 @@ class Builder implements Constrainable
     {
         if ($result = $this->first([$column])) {
             return $result->get($column);
-        };
+        }
 
         return null;
     }
